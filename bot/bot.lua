4e2b38472db1534e5765dbd4b253d84b16c38a14
shahin20nice <shahin.nice20@gmail.com>
2017-03-13 15:17:42 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {166253826},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
